c5dfae910cf0f7c9d3ebd15029e910d54ca7e07d
YuriiNovykov <novikovyuri2005@gmail.com>
2024-10-01 18:54:10 +0200

    The new message

---
+++ first.java
@@ -0,0 +1,13 @@
+public class SumTwoIntegers {
+    public static void main(String[] args) {
+        // Initialize the two integers
+        int cislo1 = 2;
+        int cislo2 = 3;
+
+        // Calculate the sum
+        int vysledok = cislo1 + cislo2;
+
+        // Display the result
+        System.out.println("The sum (vysledok) is: " + vysledok);
+    }
+}
